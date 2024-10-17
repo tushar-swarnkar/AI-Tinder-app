882a0af9a6043f90b317471755cdc3ce37ca67cf
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-10-17 20:42:55 +0530

    Basic initializer app in working state

---
+++ src/main/java/io/tinder/ai_tinder_backend/AiTinderBackendApplication.java
@@ -0,0 +1,13 @@
+package io.tinder.ai_tinder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tinder/ai_tinder_backend/AiTinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tinder.ai_tinder_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
